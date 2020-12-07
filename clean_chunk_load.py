1df2d8cda7e223efb9160b4a8e593ed65d8cda0c
Jordan <jspringer@covermymeds.com>
2020-12-07 18:10:08 -0500

    cleanup

--- clean_chunk_load.py
+++ clean_chunk_load.py
@@ -1,11 +1,12 @@
-##load a csv file, remove commas and quote-encapsulation, replace with a delimiter of your choice
+## load a csv file, remove commas and quote-encapsulation, replace with a delimiter of your choice
+## outstanding: remove .csv enforce
 import pandas as pd
 from getpass import getuser
 
 print('This code will remove commas and quotes and from your .csv file and chunk it to your specifications. ')
 #vars
 i=1
-fn=input('Input file path: ')
+fn=input('Input file path: ') #format like c\desktop\foo\bar
 o_fn=input('Output file path: ')
 c_size=input('Desired chunk size: ')
 c_size=int(c_size)
